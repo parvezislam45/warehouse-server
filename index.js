816fa184853dfdf09f867c8803ec27830a2a9622
Parvez Islam <parvezislam45@gmail.com>
2022-05-02 08:24:56 +0600

    add delete post

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run(){
         const result = await productCollection.insertOne(newProduct);
         res.send(result);
     })
+
+    // ---------- Delate Data----------------
+     app.delete('/product/:id',async (req,res)=>{
+         const id = req.params.id;
+         const query = {_id: ObjectId(id)};
+         const result = await productCollection.deleteOne(query);
+         res.send(result);
+     })
     }
     finally{
 
